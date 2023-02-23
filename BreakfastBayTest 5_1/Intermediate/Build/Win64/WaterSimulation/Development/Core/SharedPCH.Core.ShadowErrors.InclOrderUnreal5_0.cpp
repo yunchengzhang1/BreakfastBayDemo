7bf6972dc90679394eb622c560233a680328ac02
yunchengzhang1 <yunchengz@smu.edu>
2023-02-23 15:27:14 -0600

    some updates
    - mat to sandstone
    - shooting camera offset changed

---
+++ BreakfastBayTest 5_1/Intermediate/Build/Win64/WaterSimulation/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30148
+#include "C:/Users/AZ/Documents/GitHub/BreakfastBayDemo/BreakfastBayTest 5_1/Intermediate/Build/Win64/WaterSimulation/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.h"
---
+++ BreakfastBayTest 5_1/Intermediate/Source/WaterSimulation.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class WaterSimulation : ModuleRules
+{
+	public WaterSimulation(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
